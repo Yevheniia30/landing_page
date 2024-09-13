28e56cd45830028c0cc189c9ae2e16c1bdd69678
dn301086siv <yevheniia.suska@privatbank.ua>
2024-09-13 22:57:38 +0300

    #tests-cra - upd

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -1,14 +1,16 @@
 import {render, screen} from '@testing-library/react';
 import Greet from './greet';
 
-test('Greet renders correctly', () => {
-    render(<Greet />);
-    const textEl = screen.getByText('Hello');
-    expect(textEl).toBeInTheDocument();
-});
+describe('Greet group', () => {
+    test('Greet renders correctly', () => {
+        render(<Greet />);
+        const textEl = screen.getByText('Hello');
+        expect(textEl).toBeInTheDocument();
+    });
 
-test('Greet with name renders correctly', () => {
-    render(<Greet name="Manu" />);
-    const textEl = screen.getByText('Hello Manu');
-    expect(textEl).toBeInTheDocument();
+    it.only('Greet with name renders correctly', () => {
+        render(<Greet name="Manu" />);
+        const textEl = screen.getByText('Hello Manu');
+        expect(textEl).toBeInTheDocument();
+    });
 });
